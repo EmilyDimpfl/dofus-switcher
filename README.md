1d873bf1b3b32ceb76331c6e0e9e024f10f77b1e
Emily Dimpfl <emily.dimpfl@gmail.com>
2025-02-18 23:21:32 -0800

    Cleaned up KDE version a bit.

--- README.md
+++ README.md
@@ -5,6 +5,51 @@ This is a simple repository for holding some utility scripts I've been working o
 
 Among them are (currently):
 - `x11_switcher.py`, a script for switching between Dofus windows while using the X11 Window System.
+- `kde/`, which contains scripts for running on KDE using Wayland.
 
-I want to rebuild `x11_switcher.py` for Wayland, as I plan to switch to that in the not-too-distant future now that explicit sync drivers are available.
+## KDE Wayland
 
+Dependencies:
+
+**kdotool**: https://aur.archlinux.org/packages/kdotool-git
+
+Packages:
+```bash
+sudo pacman -S python-evdev python-pydbus python-gobject
+```
+
+The Wayland security model makes it so we can't listen to key presses from arbitrary applications. This is generally a really good thing! But it does make it harder to write this sort of window switcher. Thus, there are several parts to make this work:
+
+### `org.saone.dofusswitcher.conf`:
+
+This file has a dbus configuration to allow my user as well as `root` access a DBus System Bus for communication. On my system, it gets copied to `/usr/share/dbus-1/system.d/org.saone.dofusswitcher.conf`, followed by a system reboot. 
+
+### `kdot_switcher.py`
+
+This file provides a DBus Service with an extremely simple interface - it takes strings sent from `evdev_pipe.py`, and uses `kdotool` to give windows focus.
+
+This has to be run _after_ setting up the above config file, should be started _before_ the `evdev_pipe.py` (though it isn't mandatory).
+
+Run via:
+```bash
+python3 kdot_switcher.py
+```
+
+### `evdev_pipe.py`
+
+This file takes a list of keys to listen to and an input device, and connects to the DBus service in `kdot_switcher.py`, and sends key events to that service. In order to avoid this simply being a keylogger, it only sends the six specific keys needed for `kdot_switcher.py` to work.
+
+Because this service is reading from a `/dev/input/` device, it needs root access to run. Feel free to check [the code](kde/evdev_pipe.py) - it's pretty readable to verify you're not just running a keylogger.
+
+Run via:
+```bash
+sudo python3 evdev_pipe.py
+```
+
+## TODO:
+
+I'd like to make these pick up configs from .ini files, or command line options or something. Right now, I'm just focusing on getting things basically working.
+
+I could eventually make this into a package that automatically sets things up based on your system and such, but I'm still building out basic functionality for me to use, so volunteers are welcome to chat with me or submit a PR.
+
+Supposedly Ankama will be building in a window switcher to Dofus Unity eventually, but I need my window switcher in the meantime.
--- kde/evdev_pipe.py
+++ kde/evdev_pipe.py
@@ -2,30 +2,43 @@
 # This script should be run as root, and will send specific keyboard events to a
 #  userspace program via dbus.
 from pathlib import Path
-import time
-import pydbus
+from time import sleep
+from pydbus.bus import Bus
+from pydbus import SystemBus
 from evdev import InputDevice, categorize, ecodes, list_devices
+
+from gi.repository.GLib import GError
 
 # Keys to pipe:
 listen_keys = ["KEY_F4", "KEY_F5", "KEY_F6", "KEY_F7", "KEY_F8", "KEY_PAUSE"]
+# Device to listen to:
+input_device = "/dev/input/by-id/usb-DuckyChannel_International_Co.__Ltd._Ducky_Keyboard-event-kbd"
+
 
 # dbus stuff:
-def setup_dbus() -> pydbus.bus.Bus:
+def setup_dbus() -> Bus:
     # Set up our system bus.
-    bus = pydbus.SystemBus()
+    bus = SystemBus()
 
     # Define the service, path, and interface
     service_name = "org.saone.dofusswitcher"
 
     # Register the object on the bus
     bus_obj = bus.get(service_name)
-    
+
     return bus_obj
 
+def retry_setup_dbus() -> Bus:
+    # get dbus interface to send on:
+    while True:
+        try:
+            interface = setup_dbus()
+            return interface
+        except GError:
+            # error connecting to the dbus
+            print("Error: unable to connect to bus.")
+        sleep(5)
 
-def emit_key_event(bus: pydbus.bus.Bus, key_name: str) -> None:
-    # Emit a signal on the dbus bus when a key is pressed
-    bus.KeyPressed(key_name)
 
 def get_keyboard(input_device: str) -> InputDevice:
     input_device = str(Path(input_device).resolve())
@@ -43,29 +56,35 @@ def get_keyboard(input_device: str) -> InputDevice:
 
     if not keyboard:
         print("No keyboard device found!")
-        exit()
+        exit(1)
     return keyboard
 
-def main():
-    # Device to listen to:
-    input_device = "/dev/input/by-id/usb-DuckyChannel_International_Co.__Ltd._Ducky_Keyboard-event-kbd"
-    # todo: provide this via argparse or env var or something
-    keyboard = get_keyboard(input_device)
 
-    # get dbus interface to send on:
-    interface = setup_dbus()
-
+def relay_keypresses(kb: InputDevice, bus: Bus):
     print("Listening for key presses...")
+    for event in kb.read_loop():
+        if event.type == ecodes.EV_KEY and event.value == 1:  # Key press (value 1)
+            key_event = categorize(event)
+            key_name = key_event.keycode
+            # only listen for specific keys:
+            if key_name in listen_keys:
+                # Emit a signal on the dbus bus when a key is pressed
+                bus.KeyPressed(key_name)
+        sleep(0.01)  # cede CPU
+
+
+def main():
     try:
-        for event in keyboard.read_loop():
-            if event.type == ecodes.EV_KEY and event.value == 1:  # Key press (value 1)
-                key_event = categorize(event)
-                key_name = key_event.keycode
-                if key_name in listen_keys: # only listen for specific keys
-                    emit_key_event(interface, key_name)
-            time.sleep(0.01) # cede CPU
+        # todo: provide this via argparse or env var or something
+        keyboard = get_keyboard(input_device)
+
+        # get dbus interface to send on:
+        interface = retry_setup_dbus()
+
+        relay_keypresses(keyboard, interface)
     except KeyboardInterrupt:
-        print("Shutting down...")
+        print("\nShutting down...")
+
 
 if __name__ == "__main__":
     main()
